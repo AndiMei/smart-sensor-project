bb3a04e4f990a9ff8349053cfcd445b93aa7d688
Andi Mei <andimendik@gmail.com>
2019-11-08 10:20:18 +0700

    Penambahan program pada main.c

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 /*
  * main.c
  *
- *  Created on: Nov 8, 2019
+ *  Created on: Sep 13, 2019
  *      Author: Andi
  */
+
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+//#include "mySPI/mySPI.h"
+//#include "myADC/myADC.h"
+//#include "myUART/myUART.h"
+
+char buff[100];
+int i=0;
+
+int main(void){
+//	ADC_Init();
+	DDRA =0xFF;
+//	SPI_MasterInit();
+//	USART_Init();
+	while(1){
+
+//		buff[i] = SPI_MasterTransmit(0x01);
+//		USART_PutChar(buff[i]);
+//		if(buff[i] == '\0'){
+//			USART_PutStr("\n");
+//			i=0;
+//		}
+//		i++;
+//
+//		_delay_ms(1);
+	}
+}
